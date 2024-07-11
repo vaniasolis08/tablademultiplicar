79e311451ac1a6350cdc61e943f9fa632b35f6c4
vania solis <solisvictoria924@gmail.com>
2024-07-11 12:06:14 -0600

    iniciado

---
+++ src/tablade/multipilcar/pkg10/TabladeMultipilcar10.java
@@ -0,0 +1,17 @@
+
+package tablade.multipilcar.pkg10;
+
+
+public class TabladeMultipilcar10 {
+
+   
+    public static void main(String[] args) {
+        int numero = 10;
+        int total = 0;
+        for (int i = 1; i < 10; i++) {
+            total = numero *i;
+            System.out.println(numero + "*" + i + "=" + total);
+        }
+    }
+    
+}
